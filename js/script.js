2a225f2423b02f11bb7bcaea18912c04e4e2841d
kmbrandt94 <kmbrandt94@gmail.com>
2016-10-12 12:43:27 -0500

    Late Javascript Assignment 5

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Be sure to add an IIFE or jQuery ready to scope your work!
+$(document).ready(function () {
 
+    $('aside').removeClass('hide');
+    var featuredItems = '#featured-items';
+    var createItems = function (name, image, description) {
+        $(featuredItems).append('<h3>' + name + '</h3>');
+        $(featuredItems).append('<img src="' + image + '">');
+        $(featuredItems).append('<p>' + description + '</p>');
+        $(featuredItems).append('<br>');
+    }
 
+    $.getJSON('items.json', function (data) {
+        var i;
+        var item;
+        for (i = 0; i < data.menuItems.length; i++) {
+            item = data.menuItems[i];
+            createItems(item.name, item.image, item.description);
+        }
 
-
+    });
+});
